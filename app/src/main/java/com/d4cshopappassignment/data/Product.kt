20140694f51e2197aaf1dd9491c8e5602f79ce0f
Mohd Raghib <raghib.khan76@gmail.com>
2025-05-25 02:57:30 +0530

    feat: incoming mock data and category bg

--- app/src/main/java/com/d4cshopappassignment/data/Product.kt
+++ app/src/main/java/com/d4cshopappassignment/data/Product.kt
@@ -5,7 +5,7 @@ import com.checkmycharger.shopappassignment.R
 data class Product(
     val title: String,
     val description: String,
-    val secondDescription: String = "Checking",
+    val secondDescription: String = "Dry & Dehydrated skin",
     val price: String,
     val originalPrice: String,
     val imageRes: Int,
@@ -18,18 +18,18 @@ val sampleProducts = listOf(
     Product(
         title = "Clencera",
         description = "Gentle face cleanser for daily use.",
-        price = "$19.99",
-        originalPrice = "$24.99",
+        price = "RS. 19.99",
+        originalPrice = "RS. 24.99",
         imageRes = R.drawable.product_image,
         isBestSeller = true,
         inStock = true,
-        reviewsCount = 249
+        reviewsCount = 144
     ),
     Product(
         title = "Glow",
         description = "Vitamin C serum for brighter skin.",
-        price = "$29.99",
-        originalPrice = "$39.99",
+        price = "RS. 29.99",
+        originalPrice = "RS. 39.99",
         imageRes = R.drawable.product_image,
         isBestSeller = false,
         inStock = true,
@@ -38,8 +38,8 @@ val sampleProducts = listOf(
     Product(
         title = "AfterGlow",
         description = "Nourishing night cream for hydration.",
-        price = "$34.99",
-        originalPrice = "$44.99",
+        price = "RS. 34.99",
+        originalPrice = "RS. 44.99",
         imageRes = R.drawable.product_image,
         isBestSeller = true,
         inStock = false,
