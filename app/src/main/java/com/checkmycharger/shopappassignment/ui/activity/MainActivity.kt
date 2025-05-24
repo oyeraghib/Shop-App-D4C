ee921c1fa921c67b0dcffffa14604dd4e3c2cfd6
Mohd Raghib <raghib.khan76@gmail.com>
2025-05-24 23:12:52 +0530

    add intent to go to MainActivity.kt

--- app/src/main/java/com/checkmycharger/shopappassignment/ui/activity/MainActivity.kt
+++ app/src/main/java/com/checkmycharger/shopappassignment/ui/activity/MainActivity.kt
@@ -1,32 +1,26 @@
 package com.checkmycharger.shopappassignment.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.checkmycharger.shopappassignment.ui.activity.shopHome.HomeActivity
 import com.checkmycharger.shopappassignment.ui.theme.ShopAppAssignmentTheme
+import java.util.logging.Logger
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ShopAppAssignmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        Logger.getLogger("main").info("MainActivity started")
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+
+        finish()
     }
 }
 
